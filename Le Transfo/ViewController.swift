206281c79eafffb57ebf2707ad5e032d0d11abcd
Vavelin Kevin <vavelinkevin@gmail.com>
2015-03-06 04:07:38 +0400

    Initial Commit

---
+++ Le Transfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Le Transfo
+//
+//  Created by Vavelin Kevin on 06/03/15.
+//  Copyright (c) 2015 Vavelin Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
